4b3953cde80dec9afc5db454e41fe9ad4e5fc04e
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2022-04-18 22:04:45 +0700

    init new project

---
+++ reactive-mongo-crud/src/main/java/com/playground/reactivemongocrud/ReactiveMongoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.playground.reactivemongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoCrudApplication.class, args);
+	}
+
+}
---
+++ reactive-mongo-crud/src/test/java/com/playground/reactivemongocrud/ReactiveMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.reactivemongocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
